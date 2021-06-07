ff5c83a04506af64ea49e80f2baddc06354c4d1a
VishakhaSingh07 <68512366+VishakhaSingh07@users.noreply.github.com>
2021-06-07 22:31:40 +0530

    Add files via upload

---
+++ Note2.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Note2() {
+  return (
+    <div className="note">
+      <h1>Flutter</h1>
+      <p>
+        Flutter is an open-source UI software development kit created by Google.
+        It is used to develop cross platform applications for Android, iOS,
+        Linux, Mac, Windows, Google Fuchsia, and the web from a single codebase.
+        The first version of Flutter was known as codename "Sky" and ran on the
+        Android operating system.
+      </p>
+    </div>
+  );
+}
+export default Note2;
---
+++ Note4.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Note4() {
+  return (
+    <div className="note">
+      <h1>Angular</h1>
+      <p>
+        Angular is a TypeScript-based open-source web application framework led
+        by the Angular Team at Google and by a community of individuals and
+        corporations. Angular is a complete rewrite from the same team that
+        built AngularJS.
+      </p>
+    </div>
+  );
+}
+export default Note4;
